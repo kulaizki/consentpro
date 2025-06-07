d0f391f5a54ce00906856d414e4c45463f2f72b7
kulaizki <kulaizke@gmail.com>
2025-06-08 01:33:05 +0800

    style: update icon in HowItWorks component for improved visual representation

--- src/lib/components/HowItWorks.svelte
+++ src/lib/components/HowItWorks.svelte
@@ -28,7 +28,7 @@
 			name: 'Report with Confidence',
 			description:
 				'Generate detailed reports for internal audits or regulatory reviews to effortlessly demonstrate compliance.',
-			icon: 'M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.57-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.286zm0 13.036h.008v.008h-.008v-.008z'
+			icon: 'M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z'
 		}
 	];
 </script>
